127dd8c81168ddff8e98b313d2da202db8aad21d
Jonny Ingall <jonny@mondago.com>
2024-01-31 11:49:37 +0000

    Allow specifying a container name when not in a project dir

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,37 @@ use std::{self, env, fs, process::Command};
 use yaml_rust::YamlLoader;
 
 fn main() {
+    crossterm::terminal::enable_raw_mode().unwrap();
+
     let cwd = env::current_dir().expect("Could not read cwd");
     let file: String =
         match fs::read_to_string(format!("{}/docker-compose.yml", cwd.to_string_lossy())) {
             Ok(v) => v,
             Err(_) => fs::read_to_string(format!("{}/compose.yml", cwd.to_string_lossy()))
-                .expect("Could not find a compose file"),
+                .unwrap_or("".to_string()),
         };
 
-    let yaml = &YamlLoader::load_from_str(&file).unwrap()[0];
-
-    let container_name = yaml["services"]["web"]["container_name"]
-        .as_str()
-        .expect("The expected keys were not found in the compose file");
+    let yamls = &YamlLoader::load_from_str(&file).unwrap();
+    
+    let container_name = match yamls.get(0) {
+        Some(yaml) => yaml["services"]["web"]["container_name"].as_str().unwrap_or(""),
+        None => ""
+    };
 
     let mut args: Vec<_> = env::args().collect();
     args.remove(0);
 
-    crossterm::terminal::enable_raw_mode().unwrap();
+    let mut command = Command::new("docker");
 
-    let mut child = Command::new("docker")
-        .arg("exec")
-        .arg("-it")
-        .arg(container_name)
-        .args(args)
-        .spawn()
+    command.arg("exec")
+        .arg("-it");
+
+    if container_name != "" {
+        command.arg(container_name);
+    }
+    command.args(args);
+
+    let mut child = command.spawn()
         .expect("Failed to execute");
 
     child.wait().unwrap();
